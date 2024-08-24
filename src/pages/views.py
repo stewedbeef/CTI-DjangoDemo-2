b37d6775dc6eba84dbba9ffe9664f304295e4518
stewedbeef <114018527+stewedbeef@users.noreply.github.com>
2024-08-24 02:47:46 -0700

    Add TODO page

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(*args, **kwargs):
 	return HttpResponse("<h1>Hello world!</h1>")
+
+def todo_view(*args, **kwargs):
+	return HttpResponse("<h1>TODO</h1><p>Work in progress</p>")
